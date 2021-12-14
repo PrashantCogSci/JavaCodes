eddc4e4e152f385be5aabcfcf56a4768260db0fe
PrashantCogSci <pandey.prashant09@outlook.com>
2021-12-14 16:00:26 +0530

    done

---
+++ src/GitLearning/GitBasic.java
@@ -0,0 +1,7 @@
+package GitLearning;
+
+public class GitBasic {
+    public static void main(String[] args) {
+        System.out.println("Leraning and testing the code");
+    }
+}
---
+++ src/GitLearning/JustCheck.java
@@ -0,0 +1,7 @@
+package GitLearning;
+
+public class JustCheck {
+    public static void main(String[] args) {
+        System.out.println("To check after Value");
+    }
+}
