6ece3b81efd5c4208ae834436826df4f62f3eb81
PrashantCogSci <pandey.prashant09@outlook.com>
2021-12-16 13:59:00 +0530

    done

---
+++ src/JavaBasic/DataStrorage/DataTypes.java
@@ -0,0 +1,78 @@
+package JavaBasic.DataStrorage;
+//Data types specify the different sizes and values that can be stored in the variable. There are two types of data types in Java:
+//
+//Primitive data types: The primitive data types include boolean, char, byte, short, int, long, float and double.
+//Non-primitive data types: The non-primitive data types include Classes,Interfaces and Arrays
+
+public class DataTypes {
+    public static void main(String[] args) {
+
+    }
+        boolean A = true;
+        char b = 'a';
+        byte c = 44;
+        short  d = 444;
+        int e = 444;
+        long f = 568653l;
+        float g = 55.9f;
+        double h = 44.55;
+}
+
+//Max and Min values of data types
+
+class  IntMax{
+    public static void main(String[] args) {
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+        System.out.println(Integer.MAX_VALUE+1);
+        System.out.println(Integer.MIN_VALUE-1);
+    }
+}
+
+
+class  bytetMax{
+    public static void main(String[] args) {
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+        System.out.println(Byte.MAX_VALUE+1);
+        System.out.println(Byte.MIN_VALUE-1);
+    }
+}
+
+class  ShorttMax{
+    public static void main(String[] args) {
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+        System.out.println(Short.MAX_VALUE+1);
+        System.out.println(Short.MIN_VALUE-1);
+    }
+}
+
+
+class  LongtMax{
+    public static void main(String[] args) {
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+        System.out.println(Long.MAX_VALUE+1);
+        System.out.println(Long.MIN_VALUE-1);
+    }
+}
+
+
+class  FloatMax{
+    public static void main(String[] args) {
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Float.MAX_VALUE+1);
+        System.out.println(Float.MIN_VALUE-1);
+    }
+}
+
+class  dubbleMax{
+    public static void main(String[] args) {
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE+1);
+        System.out.println(Double.MIN_VALUE-1);
+    }
+}
---
+++ src/JavaBasic/SimpleHello.java
@@ -0,0 +1,7 @@
+package JavaBasic;
+
+public class SimpleHello {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+    }
+}
